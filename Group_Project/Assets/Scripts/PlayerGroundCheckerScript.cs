c3764ccb035372c8d807311c9b8cd1ce43527614
SoleSaken <yufeng132@gmail.com>
2023-02-17 15:12:29 +0800

    Fixing past accident repo and physics, Added Grounded Script for player.
    Don't do commits to main branch, make a new branch and bring your changes over to your own branch, only merge with pull request. (When someone is ready for merge)
    
    You can name your own branch anything you want, work-related or by your name/nickname.

---
+++ Group_Project/Assets/Scripts/PlayerGroundCheckerScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundCheckerScript : MonoBehaviour
+{
+    public PlayerScript playerScript;
+    public bool groundedCheck = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (groundedCheck)
+        {
+            groundedCheck = false;
+        }
+        else
+        {
+            playerScript.onGround = false;
+            Debug.Log("air");
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject != playerScript.gameObject)
+        {
+            if (!groundedCheck)
+            {
+                groundedCheck = true;
+                playerScript.onGround = true;
+
+                Debug.Log("ground");
+            }
+        }
+    }
+}
---
+++ Group_Project/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int speed;
+    public bool onGround;
+    public SpriteAnimatorScript spriteAnimatorScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteAnimatorScript = GetComponent<SpriteAnimatorScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody>().velocity -= new Vector3(speed * Time.deltaTime, 0, 0);
+            GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody>().velocity += new Vector3(speed * Time.deltaTime, 0, 0);
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (onGround && GetComponent<Rigidbody>().velocity.y <= 0)
+            {
+                GetComponent<Rigidbody>().velocity += new Vector3(0, 5, 0);
+                onGround = false;
+                spriteAnimatorScript.ChangeAnimation(3);
+            }
+        }
+
+        if (onGround && GetComponent<Rigidbody>().velocity.y <= 0)
+        {
+            if (GetComponent<Rigidbody>().velocity.x > 0.2f || GetComponent<Rigidbody>().velocity.x < 0.2f && GetComponent<Rigidbody>().velocity.x != 0f)
+            {
+                spriteAnimatorScript.ChangeAnimation(1);
+            }
+            else
+            {
+                spriteAnimatorScript.ChangeAnimation(0);
+            }
+        }
+
+    }
+}
