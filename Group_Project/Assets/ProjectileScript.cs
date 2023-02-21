399331f01fd00569f445055915b23d8335ccaac2
Oliver <olivercho@rocketmail.com>
2023-02-21 12:23:50 +0800

    Level Design Finished
    Hitbox,Level Design and Lazer turrets work.
    still need code in death and win

---
+++ Group_Project/Assets/ProjectileScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileScript : MonoBehaviour
+{
+    [HideInInspector]
+    public float speed;
+    [HideInInspector]
+    public float lifetime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (lifetime > 0)
+        {
+            lifetime -= Time.fixedDeltaTime;
+
+            transform.position += transform.right * speed * Time.fixedDeltaTime;
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Group_Project/Assets/Scripts/Lazershoot.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lazershoot : MonoBehaviour
+{
+    public GameObject lazerProjectilePrefab;
+    public float rotationDirection;
+
+    [Space]
+
+    public float speed;
+    public float lifetime;
+    public float shotCD;
+
+    private float spawnCD = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (spawnCD > 0)
+        {
+            spawnCD -= Time.fixedDeltaTime;
+        }
+        else
+        {
+            GameObject tempedit = Instantiate(lazerProjectilePrefab, transform.position, Quaternion.identity);
+            tempedit.transform.Rotate(new Vector3(0, 0, rotationDirection));
+            tempedit.GetComponent<ProjectileScript>().speed = speed;
+            tempedit.GetComponent<ProjectileScript>().lifetime = lifetime;
+
+            spawnCD += shotCD;
+        }
+    }
+}
